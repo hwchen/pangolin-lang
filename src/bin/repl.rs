d74e16afa2e804132866023fb00729e1ba0d6dac
Walther Chen <walther.chen@gmail.com>
2019-09-28 19:27:17 -0400

    [repl] initial repl

---
+++ src/bin/repl.rs
@@ -0,0 +1,36 @@
+use pangolin_lang::{
+    lexer::Lexer,
+    token::Token,
+};
+use std::io::{self, Write};
+
+const PROMPT: &str = ">> ";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut line_buf = String::new();
+
+    loop {
+        print!("{}", PROMPT);
+        io::stdout().flush()?;
+
+        match io::stdin().read_line(&mut line_buf) {
+            Ok(_) => {
+                // TODO important, remove this clone.
+                let mut lexer = Lexer::from_str(line_buf.clone());
+
+                loop {
+                    let token = lexer.next_token();
+
+                    if token == Token::Eof {
+                        println!("  {:?}", token);
+                        break;
+                    } else {
+                        println!("  {:?}", token);
+                    }
+                }
+            },
+            Err(err) => eprintln!("Could not read line: {}", err),
+        }
+    }
+    // Ok(()) unreachable
+}
